7adec7a750b89a63806c11f767a12ad7b4a6158f
André Gonçalves da Silva <andre.godasi@gmail.com>
2017-07-19 00:24:49 -0300

    capitulo 2

---
+++ client/app/foto/foto.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: './app/foto/',
+    selector:'foto',
+    templateUrl:'./foto.component.html'
+})
+export class FotoComponent{}
