e6608535c02d61115cc1cac6e0b58a7ad3245b43
andregodasi <andre.godasi@gmail.com>
2017-07-19 12:03:50 -0300

    capitulo 3 finalizado

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http} from '@angular/http';
 
 @Component({
     moduleId: module.id,
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
     templateUrl:'./app.component.html'
 })
 export class AppComponent{
-
+    fotos:Object[] = [];
+    
+    constructor(http:Http){
+       
+        http
+        .get('v1/fotos')
+        .map(res => res.json())
+        .subscribe(fotos=>{
+            this.fotos = fotos;
+            console.log(this.fotos)
+        },erro => console.log(erro));
+    }
 }
