cedeb14aceb440a8d9ad53171bda105d01983b6c
andregodasi <andre.godasi@gmail.com>
2017-07-21 16:46:17 -0300

    curso 2 capítulo 6 concluído

---
+++ client/app/botao/botao.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'botao',
+    templateUrl: './botao.component.html'
+})
+
+export class BotaoComponent{
+    @Input() nome:String = 'Ok';
+    @Input() estilo:String = 'btn-default';
+    @Input() tipo:String = 'button';
+    @Input() desabilitado:boolean = false;
+    @Output() acao = new EventEmitter();
+    @Input() confirmacao:boolean;
+
+    executaAcao(){
+        if(this.confirmacao){
+            if(confirm('Tem certeza ?')){
+                this.acao.emit(null);
+            }
+            return;
+        }
+
+        this.acao.emit(null);
+    }
+}
+
