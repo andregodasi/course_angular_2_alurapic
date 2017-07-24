cbc7c8ae89412f0d6282295cb9331226d2131ff3
André Gonçalves da Silva <andre.godasi@gmail.com>
2017-07-23 23:53:35 -0300

    curso 2 capítulo 7 concluído, curso finalizado !!!

--- client/app/listagem/listagem.component.ts
+++ client/app/listagem/listagem.component.ts
@@ -1,3 +1,4 @@
+import { PainelComponent } from './../painel/painel.component';
 import { FotoService } from './../foto/foto.service';
 import { FotoComponent } from './../foto/foto.component';
 import { Component } from '@angular/core';
@@ -19,15 +20,17 @@ export class ListagemComponent{
         ,erro => console.log(erro));
     }
 
-    remove(foto:FotoComponent){
+    remove(foto:FotoComponent, painel:PainelComponent){
         this.service.remove(foto)
             .subscribe(
                 () => {
-                    let novasFotos = this.fotos.slice(0);
-                    let indice = novasFotos.indexOf(foto);
-                    novasFotos.splice(indice,1);
-                    this.fotos = novasFotos;
-                    this.mensagem = 'Foto removida com sucesso.';
+                    painel.fadeOut(()=>{
+                        let novasFotos = this.fotos.slice(0);
+                        let indice = novasFotos.indexOf(foto);
+                        novasFotos.splice(indice,1);
+                        this.fotos = novasFotos;
+                        this.mensagem = 'Foto removida com sucesso.';
+                    });
                 },
                 erro => {
                     console.log(erro);
